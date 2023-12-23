3ff98ff077f821bbca3aca067dff7c77725ba4ed
mohammad saleh <mohammadsalehmohammadsaleh@gmail.com>
2023-12-24 00:56:37 +0330

    اضافه شدن نمودار میله‌ای به نمرات دانش‌آموزان

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import plotly.express as px
 sheet_id = "1vIV2Sl1Mad9e-lf24-iVqXs6gwuRij-bOUl7Iir2hQ8"
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 
-fig1 = px.line(df, x='y', y='x2', color='x3' ,animation_frame='x1')
+fig1 = px.bar(df, x='y', y='x2', color='x3' ,animation_frame='x1', barmode='group',)
 st.plotly_chart(fig1, use_container_width=True)
 
 fig2 = px.line(df, x='x3', y='x2' ,color='y' ,animation_frame='x1')
